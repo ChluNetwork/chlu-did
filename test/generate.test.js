2cdaf5dd25d1391b61f3e74c91b1f20d95b0dd43
Kulpreet Singh <zapfmann@gmail.com>
2018-04-29 09:54:35 +0100

    Add key generation method

--- test/generate.test.js
+++ test/generate.test.js
@@ -1,7 +1,16 @@
 import Generator from '../lib/generator'
 
+test('generate key pair', () => {
+  const generator = new Generator()
+  const kp = generator.generateEd25519KeyPair()
+  expect(kp).not.toBe(undefined)
+
+  expect(kp.publicKey).not.toBe(null)
+  expect(kp.privateKey).not.toBe(null)
+});
+
+
 test('generate a new did', () => {
-  console.log(Generator)
   const generator = new Generator()
   const did = generator.generate()
   expect(did).not.toBe(undefined)

86b6a9782064e568fc7ee5a09ae9099e54804fff
Kulpreet Singh <zapfmann@gmail.com>
2018-04-29 20:16:55 +0100

    DID with publicKey and authentication specified

--- test/generate.test.js
+++ test/generate.test.js
@@ -20,8 +20,19 @@ it('generates a new did', async () => {
   expect(did.privateDidDocument).toBe(null)
   
   expect(did.publicDidDocument['@context']).toBe('https://w3id.org/did/v1')
+
   expect(did.publicDidDocument.id).toEqual(expect.anything())
   expect(did.publicDidDocument.id).toEqual(expect.stringMatching(/did:chlu:.*/))
   expect(did.publicDidDocument.id).not.toEqual("did:chlu:undefined")
   expect(did.publicDidDocument.id).not.toEqual("did:chlu:null")
+
+  const publicKeyBase58 = did.publicDidDocument.id.split(':')[2]
+  expect(did.publicDidDocument.publicKey).toEqual(expect.anything())
+  expect(did.publicDidDocument.publicKey[0].id).toEqual(`${did.publicDidDocument.id}#keys-1`)
+  expect(did.publicDidDocument.publicKey[0].type).toEqual("Ed25519VerificationKey2018")
+  expect(did.publicDidDocument.publicKey[0].owner).toEqual(`${did.publicDidDocument.id}`)
+  expect(did.publicDidDocument.publicKey[0].publicKeyBase58).toEqual(publicKeyBase58)
+
+  expect(did.publicDidDocument.authentication).toEqual(expect.anything()) 
+  expect(did.publicDidDocument.authentication[0].publicKey).toEqual(`did:chlu:${publicKeyBase58}#keys-1`)
 })

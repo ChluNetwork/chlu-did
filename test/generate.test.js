dc40d7ad009bb5acad57c227a005400c82b7d805
Kulpreet Singh <zapfmann@gmail.com>
2018-04-29 19:25:54 +0100

    Initial simple DID generation, with no DID Method specified

--- test/generate.test.js
+++ test/generate.test.js
@@ -1,20 +1,27 @@
 import Generator from '../lib/generator'
 
-test('generate key pair', () => {
+it('generates key pair', async () => {
   const generator = new Generator()
-  const kp = generator.generateEd25519KeyPair()
-  expect(kp).not.toBe(undefined)
+  const kp = await generator.generateEd25519KeyPair()
 
+  expect(kp).not.toBe(undefined)  
   expect(kp.publicKey).not.toBe(null)
   expect(kp.privateKey).not.toBe(null)
-});
+})
 
 
-test('generate a new did', () => {
+it('generates a new did', async () => {
+
   const generator = new Generator()
-  const did = generator.generate()
+  const did = await generator.generate()
   expect(did).not.toBe(undefined)
-
+  
   expect(did.publicDidDocument).not.toBe(null)
   expect(did.privateDidDocument).toBe(null)
-});
+  
+  expect(did.publicDidDocument['@context']).toBe('https://w3id.org/did/v1')
+  expect(did.publicDidDocument.id).toEqual(expect.anything())
+  expect(did.publicDidDocument.id).toEqual(expect.stringMatching(/did:chlu:.*/))
+  expect(did.publicDidDocument.id).not.toEqual("did:chlu:undefined")
+  expect(did.publicDidDocument.id).not.toEqual("did:chlu:null")
+})

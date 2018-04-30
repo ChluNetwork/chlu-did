b92fba0733b1eb0d96a03e35662389f2c853b13d
Kulpreet Singh <zapfmann@gmail.com>
2018-04-30 13:50:53 +0200

    Use jsonld-signatures to sign reviews

--- test/review.test.js
+++ test/review.test.js
@@ -5,21 +5,28 @@ const chluDID = {"@context":"https://w3id.org/did/v1","id":"did:chlu:B6BrdJTTCzu
 const vendorDID = {"@context":"https://w3id.org/did/v1","id":"did:chlu:GuhduVevhGzwc8DtyNQWoUUQvSWcAZrTuNqNL9bG7XfY","publicKey":[{"id":"did:chlu:GuhduVevhGzwc8DtyNQWoUUQvSWcAZrTuNqNL9bG7XfY#keys-1","type":"Ed25519VerificationKey2018","owner":"did:chlu:GuhduVevhGzwc8DtyNQWoUUQvSWcAZrTuNqNL9bG7XfY","publicKeyBase58":"GuhduVevhGzwc8DtyNQWoUUQvSWcAZrTuNqNL9bG7XfY","privateKeyBase58":"4sGHzpG5th38v4WhaShoXRKaDvN3vtXgXqQjCwcqs19xetAhkbabBitKFPPgZnrGESQaQrfdeTbqZjbzh7C6sZaS"}],"authentication":[{"type":"Ed25519SignatureAuthentication2018","publicKey":"did:chlu:GuhduVevhGzwc8DtyNQWoUUQvSWcAZrTuNqNL9bG7XfY#keys-1"}]}
 
 fit('Signs a review, returning a verifiable claim', async () => {
+  
   const review = {
-    review: 'Sample review text',
-    rating: 4
+        ReviewBody: "blah",
+        ReviewRating: 5
   }
-{
-  "id": "http://chlu.io/credentials/1",
-  "type": ["Credential"],
-  "issuer": "https://dmv.example.gov",
-  "issued": "2010-01-01",
-  "claim": {
-    "id": "did:ebfeb1f712ebc6f1c276e12ec21",
-    "ageOver": 21
-  }
-}
-  const vc = new Review().sign(chluDID, vendorDID, review)
+  // {
+  //   "id": "http://chlu.io/credentials/1",
+  //   "type": ["Credential"],
+  //   "issuer": "https://dmv.example.gov",
+  //   "issued": "2010-01-01",
+  //   "claim": {
+  //     "id": "did:ebfeb1f712ebc6f1c276e12ec21",
+  //     "ageOver": 21
+  //   }
+  // }
+  
+  const vc = await new Review().sign(chluDID, vendorDID, review)
 
   expect(vc).not.toBe(undefined)
+  expect(vc.subject).toBe(vendorDID.id)
+  expect(vc.issuer).toBe(chluDID.id)
+  expect(vc.claim).toEqual(review)
+  expect(vc['sec:proof']).not.toBe(null)
+  expect(vc['sec:proof']).not.toBe(undefined)
 })

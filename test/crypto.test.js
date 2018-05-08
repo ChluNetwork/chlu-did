469a99f3518065e1b328b307e0a4ce25160ebe61
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-05-08 16:05:32 +0200

    tests and bugfixes for signing/verifying

---
+++ test/crypto.test.js
@@ -0,0 +1,15 @@
+const ChluDID = require('../')
+const expect = require('chai').expect
+
+describe('DID signing', () => {
+
+    it('can sign and verify data', async () => {
+        const DID = new ChluDID()
+        const did = await DID.generateDID() 
+        const toSign = 'abcdef'
+        const output = await DID.sign(did.privateKeyBase58, toSign)
+        const valid = await DID.verify(did.publicDidDocument, output.data, output.signature)
+        expect(valid).to.be.true
+    })
+
+})
